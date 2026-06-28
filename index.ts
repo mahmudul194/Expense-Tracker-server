25459933a91bcfc5267524b82191084ae1946df6
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-06-28 18:21:20 +0600

    Define Expense Schema and Mongoose Model

--- index.ts
+++ index.ts
@@ -15,3 +15,10 @@ const MONGODB_URI = process.env.MONGODB_URI || '';
 mongoose.connect(MONGODB_URI)
   .then(() => console.log('Connected to MongoDB!'))
   .catch(err => console.error('Failed to connect to MongoDB:', err));
+// Create a Mongoose schema and model for our expenses
+const expenseSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  amount: { type: Number, required: true },
+  category: { type: String, required: true },
+  date: { type: String, required: true }
+});
